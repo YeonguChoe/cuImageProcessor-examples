64ebdf696ae593dfdd1f6021978b7e53b2d9e353
YeonguChoe <yeongu.choe@icloud.com>
2025-11-27 03:58:03 -0500

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,17 @@
 #include "cuImageProcessor/bitmap.cuh"
 #include "cuImageProcessor/resize.cuh"
 #include "cuImageProcessor/crop.cuh"
-#include "cuImageProcessor/rotate.cuh"
 #include "cuImageProcessor/grayscale.cuh"
+#include "cuImageProcessor/rotate.cuh"
 #include "cuImageProcessor/flip.cuh"
-
-#include <iostream>
 
 int main()
 {
+    resize("input.bmp", 500, 500);
+    crop("input.bmp", 100, 100, 200, 200);
+    grayscale("input.bmp");
+    rotate("input.bmp");
     flip_horizontal("input.bmp");
+    flip_vertical("input.bmp");
     return 0;
 }
